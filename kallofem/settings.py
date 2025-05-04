a2d8e4b526a315085fcfbf07cc9d5dac07f0bac8
Kzmkr <KOZMA.KRISTOF14@GMAIL.COM>
2025-05-04 14:21:57 +0200

    Config export settings

--- kallofem/settings.py
+++ kallofem/settings.py
@@ -89,4 +89,10 @@ ROBOTSTXT_OBEY = True
 
 # Set settings whose default value is deprecated to a future-proof value
 TWISTED_REACTOR = "twisted.internet.asyncioreactor.AsyncioSelectorReactor"
-FEED_EXPORT_ENCODING = "utf-8"
+
+FEEDS = {
+    'adat/%(name)s/%(time)s.json': {
+        'format': 'json',
+        'encoding': 'utf8',
+    }
+}
