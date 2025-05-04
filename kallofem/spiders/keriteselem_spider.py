4b70bc4a4ec3831a727615659ac80f5c1cb7d95b
Kzmkr <KOZMA.KRISTOF14@GMAIL.COM>
2025-05-04 14:15:55 +0200

    Add working spider

---
+++ kallofem/spiders/keriteselem_spider.py
@@ -0,0 +1,20 @@
+import scrapy
+
+
+class KeritesElemSpider(scrapy.Spider):
+    name = "keriteselem"
+    start_urls  = ["https://kallofem.hu/shop/group/keriteselemek?page=1"]
+    #"/shop/group/keriteselemek?page=1" URL  "/shop/group/keriteselemek" helyett, mert eltérő eredményeket ad.
+
+
+    def parse(self, response):
+        # Szűkítjük a keresést a 'div.product-group-container'-en belülre,
+        # hogy elkerüljük az ütközést a másik 'article.product-row' elemekkel
+        for prod in response.css("div.product-group-container article.product-row"):
+            yield {
+                "name": prod.xpath(".//h4/text()").get(),
+                "image": prod.xpath(".//img/@src").get(),
+                "price": prod.css(".product-price::text").get(),
+
+
+            }
